460fb2c771f12fec4a53a01996334c288d504a7d
Onkar Singh <up2066719@myport.ac.uk>
2025-10-16 11:10:33 +0100

    Completed question 1-3

--- worksheet1.py
+++ worksheet1.py
@@ -1,1 +1,25 @@
-testing 
+
+
+
+
+class Queue:
+    def __init__(self):
+        self.data = []
+        self.head = 0
+        self.tail = -1
+        self.max_size = 15
+        
+        
+    def enequeue(self, NewData):
+        print("Enqueueing " + str(NewData))
+        if self.tail == self.max_size - 1:
+            print("Queue is full")
+            raise OverflowError("Cannot insert inro a full queue")
+        self.tail += 1
+        self.data.insert(self.tail, NewData)
+        print("Now the tail is at " + str(self.tail))
+        
+    def dequeue(self):
+        if
+        
+        
